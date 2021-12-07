73fb893abd45e07e5d5a0a2205b20795cf182f8c
hallsted <hallsted@kth.se>
2021-12-07 22:16:01 +0100

    wow lägga till fixes för moomin structen

--- src/render.c
+++ src/render.c
@@ -6,34 +6,45 @@
 #include <stdint.h>   
 #include <pic32mx.h>  
 #include "game.h"
-#include "structs.h"
 
 void render_moomin(int y) {
     int i, j;
-    /*for(i = player.moominX; i > player.moominX-8; i--){
-        for(j = player.moominY; j > player.moominY - 8; j--){
+    for(i = troll.moominX; i > troll.moominX-16; i--){
+        for(j = troll.moominY-y; j > troll.moominY - y - 16; j--){
+            display_pixel(i, j);
+        }
+    }
+
+   /* for(i = 60; i > 44; i--){
+        for(j = groundlvl-y; j > (groundlvl-16)-y; j--){
             display_pixel(i, j);
         }
     }*/
+}
 
-    for(i = 60; i > 54; i--){
-        for(j = 24-y; j > 16-y; j--){
-            display_pixel(i, j);
+void clear_moomin(int y){
+    int i, j;
+    for(i = troll.moominX; i > troll.moominX-16; i--){
+        for(j = troll.moominY-y; j > troll.moominY - y - 16; j--){
+            clear_pixel(i, j);
         }
     }
 }
 
+
 void render_background() {
     int i, j;
+    groundlvl = 30;
     
     /* Horizontal lines */
     for(i = 0; i < 128; i ++) {
         display_pixel(i, 0); // Top 
         display_pixel(i, 31); // Bottom
 
-        for(j = 25; j < 27; j ++) { // Ground
+        // This is for using groundlevel above bottom line
+        /*for(j = groundlvl+1; j < 27; j ++) { // Ground
             display_pixel(i, j);
-        }
+        }*/
     }
 
     /* Vertical lines */
@@ -44,6 +55,10 @@ void render_background() {
 
     /* Misc. details */
     // Frame
+}
+
+int get_groundlvl(){
+    return groundlvl;
 }
 
 void transition() {
